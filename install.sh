c0908b0675becf8f37b36bcd5c69ee0238a7f405
Dan Denney <dandenney@Dans-MacBook-Pro-2.local>
2016-01-17 13:11:33 -0500

    Update install location of bash profile

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 ln -s ~/.dotfiles/atom ~/.atom
 ln -s ~/.dotfiles/bash ~/.bash
-ln -s ~/.dotfiles/bash_profile/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
